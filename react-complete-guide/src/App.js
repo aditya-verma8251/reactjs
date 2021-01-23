de34d23e5cb0a30f33e0f592a2ec64bee396f26d
aditya-verma8251 <aditya.verma8251@gmail.com>
2021-01-23 21:14:31 +0530

    Reverting to class based approach since it is the standard industry practice

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,77 +1,78 @@
-// import React, { Component } from 'react';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const app = () => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Staphanie', age: 26 }
-    ]
-  });
-  const switchNameHandler = () => {
-    setPersonsState({
+    ],
+    otherState: 'some other value'
+  }
+  switchNameHandler = () => {
+    // personsState.persons[0].name = "Aditya"; -> this will not work!!
+    this.setState({
       persons: [
         { name: 'Aditya', age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Staphanie', age: 27 }
       ]
-    });
-  };
-  const displayState = () => {
-    console.log(personsState);
-  };
-  return (
-    <div className="App">
-      <h1>Welcome to Aditya Verma's first React App</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <button onClick={displayState}>View state in console</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-    // <h1>Another heading</h1> -> This is not allowed since JSX should have one root element
-  );
+    })
+  }
+  displayState = () => {
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to Aditya Verma's first React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.displayState}>View state in console</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+      // <h1>Another heading</h1> -> This is not allowed since JSX should have one root element
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to Aditya Verma\'s first React App'));
+  }
 }
 
-// class App extends Component {
-//   state = {
+export default App;
+
+// import React, { useState } from 'react';
+
+// const app = () => {
+//   const [personsState, setPersonsState] = useState({
 //     persons: [
 //       { name: 'Max', age: 28 },
 //       { name: 'Manu', age: 29 },
 //       { name: 'Staphanie', age: 26 }
-//     ],
-//     otherState: 'some other value'
-//   }
-//   switchNameHandler = () => {
-//     // personsState.persons[0].name = "Aditya"; -> this will not work!!
-//     this.setState({
+//     ]
+//   });
+//   const switchNameHandler = () => {
+//     setPersonsState({
 //       persons: [
 //         { name: 'Aditya', age: 28 },
 //         { name: 'Manu', age: 29 },
 //         { name: 'Staphanie', age: 27 }
 //       ]
-//     })
-//   }
-//   displayState = () => {
+//     });
+//   };
+//   const displayState = () => {
 //     console.log(personsState);
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Welcome to Aditya Verma's first React App</h1>
-//         <button onClick={this.switchNameHandler}>Switch Name</button>
-//         <button onClick={this.displayState}>View state in console</button>
-//         <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-//         <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-//         <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-//       </div>
-//       // <h1>Another heading</h1> -> This is not allowed since JSX should have one root element
-//     );
-//     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to Aditya Verma\'s first React App'));
-//   }
+//   };
+//   return (
+//     <div className="App">
+//       <h1>Welcome to Aditya Verma's first React App</h1>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <button onClick={displayState}>View state in console</button>
+//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+//     </div>
+//     // <h1>Another heading</h1> -> This is not allowed since JSX should have one root element
+//   );
 // }
-
-export default app;
