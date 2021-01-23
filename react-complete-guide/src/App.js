3b03cc56bae0a72964c665562e105d4e4a7e5d34
aditya-verma8251 <Aditya.Verma8251@gmail.com>
2021-01-23 19:49:30 +0530

    Adding commits from Ubuntu WSL

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to Aditya Verma's first React App</h1>
+        <p>This is really working!</p>
       </div>
+      // <h1>Another heading</h1> -> This is not allowed since JSX should have one root element
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to Aditya Verma\'s first React App'));
   }
