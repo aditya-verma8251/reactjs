544d63e04755c30e5643a0c5e8849f228d9f911f
aditya-verma8251 <aditya.verma8251@gmail.com>
2021-01-24 12:58:04 +0530

    Adding code for passing method references between functions

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ],
     otherState: 'some other value'
   }
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // personsState.persons[0].name = "Aditya"; -> this will not work!!
     this.setState({
       persons: [
-        { name: 'Aditya', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Staphanie', age: 27 }
       ]
@@ -28,10 +28,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to Aditya Verma's first React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Adi')}>Switch Name</button>
         <button onClick={this.displayState}>View state in console</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click = {this.switchNameHandler.bind(this, 'Michael')}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
       // <h1>Another heading</h1> -> This is not allowed since JSX should have one root element
